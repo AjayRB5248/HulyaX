b1cc2031fba64e7f8665af0c2c4d74d2cd4ccd93
Prabesh <prabesh@Prabeshs-MacBook-Pro.local>
2024-02-09 22:47:22 +0545

    permission added

--- src/models/company.model.js
+++ src/models/company.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { PERMISSION_CONSTANTS } = require("../utility/constants");
 
 const companySchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -9,6 +10,11 @@ const companySchema = new mongoose.Schema({
   },
   description: { type: String },
   location: { type: String },
+  permissions: {
+    type: [String],
+    default: [],
+    enum: Object.values(PERMISSION_CONSTANTS),
+  },
 });
 
 const Company = mongoose.model("Company", companySchema);
